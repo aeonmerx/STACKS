aadba6cb60207ec6352662714fb6d866a372f741
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2022-09-15 14:13:49 -0500

    Add files via upload

---
+++ puntopeguelo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="Componentes">
+ <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MiComponente> </MiComponente>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from './components/MiComponente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MiComponente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#Componentes{
+  background-color: rgb(62, 202, 50);
+}
+</style>
---
+++ puntopeguelo/src/components/MiComponente.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="mi-componente">
+    <hr>
+ <h1>MI COMPONENTE</h1>
+ 
+    <h2>{{ subtitulo }}</h2>
+    <hr>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'MiComponente',
+    data(){
+        return{
+            subtitulo: 'Soy el subtitulo de mi componente'
+        }
+    }
+}
+
+</script>
